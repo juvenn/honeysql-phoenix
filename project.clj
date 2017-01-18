f7d6c933ece96e09adf3baf0b1688c5c98690c1a
Juvenn Woo <machese@gmail.com>
2017-01-18 22:27:55 +0800

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject walkingcloud/honeysql-phoenix "0.1.1-SNAPSHOT"
+(defproject walkingcloud/honeysql-phoenix "0.1.2-SNAPSHOT"
   :description "Honeysql extension for HBase Phoenix"
   :url "https://github.com/juvenn/honeysql-phoenix"
   :license {:name "Eclipse Public License"
