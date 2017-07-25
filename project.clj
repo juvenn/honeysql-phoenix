4fcac244b21693f4e3a03659a6d18404ed79b3c7
Juvenn Woo <machese@gmail.com>
2017-07-25 22:07:10 +0800

    Update jdbc to 0.7.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/java.jdbc "0.6.1"]
+                 [org.clojure/java.jdbc "0.7.0"]
                  [honeysql "0.9.0"]]
   :profiles {:dev {:dependencies [[org.apache.hbase/hbase-client "1.2.2"]
                                   [org.apache.phoenix/phoenix-core
