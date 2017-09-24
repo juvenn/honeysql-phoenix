4775f679c8190d2b6f809fe789f8c14dcdd773b5
Juvenn Woo <machese@gmail.com>
2017-09-24 13:23:50 +0800

    Release 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject honeysql-phoenix "0.2.0"
+(defproject honeysql-phoenix "0.2.1"
   :description "Clojure SQL for HBase Phoenix"
   :url "https://github.com/juvenn/honeysql-phoenix"
   :license {:name "Eclipse Public License"
